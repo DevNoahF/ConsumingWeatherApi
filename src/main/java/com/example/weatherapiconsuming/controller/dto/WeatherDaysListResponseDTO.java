ffd9ef5ae633b1a7d373465cac524610853c637a
unknown <noahvf16@outlook.com>
2026-02-14 15:46:53 -0300

    create: create ObjectMapperConfig for add @Bean for DI in weather service

--- src/main/java/com/example/weatherapiconsuming/controller/dto/WeatherDaysListResponseDTO.java
+++ src/main/java/com/example/weatherapiconsuming/controller/dto/WeatherDaysListResponseDTO.java
@@ -2,10 +2,12 @@ package com.example.weatherapiconsuming.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serializable;
+
 public record DaysListResponseDTO(@JsonProperty("tempmax") String tempMax,
                                   @JsonProperty("tempmin") String tempMin,
                                   @JsonProperty("temp") String temp,
                                   @JsonProperty("datetime") String dateTime,
                                   @JsonProperty("humidity") String humidity,
-                                  @JsonProperty("conditions") String conditions) {
+                                  @JsonProperty("conditions") String conditions) implements Serializable {
 }
