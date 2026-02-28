8a245606f15278c315bba4f58bb80280f2264f7f
noah <noahvf16@outlook.com>
2026-02-27 21:54:20 -0300

    feat: update DTOs to ignore unknown properties and enhance query parameters

--- src/main/java/com/example/weatherapiconsuming/controller/dto/WeatherDaysListResponseDTO.java
+++ src/main/java/com/example/weatherapiconsuming/controller/dto/WeatherDaysListResponseDTO.java
@@ -1,12 +1,16 @@
 package com.example.weatherapiconsuming.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
+// ele ignora campos nao declarados
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record WeatherDaysListResponseDTO(@JsonProperty("tempmax") String tempMax,
                                          @JsonProperty("tempmin") String tempMin,
                                          @JsonProperty("temp") String temp,
+                                         @JsonProperty("datetimeEpoch") String datetimeEpoch,
                                          @JsonProperty("datetime") String dateTime,
                                          @JsonProperty("humidity") String humidity,
                                          @JsonProperty("conditions") String conditions) implements Serializable {
--- src/main/java/com/example/weatherapiconsuming/controller/dto/WeatherResponseDTO.java
+++ src/main/java/com/example/weatherapiconsuming/controller/dto/WeatherResponseDTO.java
@@ -1,5 +1,6 @@
 package com.example.weatherapiconsuming.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
@@ -7,7 +8,9 @@ import java.io.Serializable;
 import java.util.List;
 
 @Builder
-public record WeatherResponseDTO(@JsonProperty("address") String adress,
+// ele ignora dados que eu nao declarei
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record WeatherResponseDTO(@JsonProperty("address") String address,
                                  @JsonProperty("timezone") String timezone,
                                  @JsonProperty("days") List<WeatherDaysListResponseDTO> days) implements Serializable {
 }
